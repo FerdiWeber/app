a0a213048d954975e4df07485f1a6be4c2b96e08
uqrjd <uqrjd@student.kit.edu>
2025-10-24 09:57:57 +0200

    added complete protocol

--- open_wearable/lib/apps/allergy_symptom_tracker/model/study_protocol.dart
+++ open_wearable/lib/apps/allergy_symptom_tracker/model/study_protocol.dart
@@ -13,13 +13,20 @@ class Dataset1Protocol extends StudyProtocol {
     StudyStep(type: StudyStepType.measuring, duration: 30, signalFrame: true, measuringTimes: [2,3]),
     StudyStep(type: StudyStepType.instruction, heading: "Kratzbewegung an den Augen", description: "Krate/Reibe deine Augen mit deinen Händen \n Tue das für 2 Sekunden, und anschließend 5 Sekunden nicht. \n Wiederhole diese Schritte."),
     StudyStep(type: StudyStepType.measuring, duration: 30),
+    StudyStep(type: StudyStepType.instruction, heading: "Husten", description: "Huste während der Aufzeichnung immer dann wenn der Rahmen des Bildschirmes Grün wird. Während der Rahmen rot ist, soll nichts getan werden"),
+    StudyStep(type: StudyStepType.measuring, duration: 30, signalFrame: true, measuringTimes: [2,3]),
+    StudyStep(type: StudyStepType.instruction, heading: "Kratzbewegung an den Ohren", description: "Wenn der Rahmen grün ist, soll mit den Händen an den Ohren gekratzt/gerieben werden. Sobald der Rahmen rot wird, soll nichts getan werden"),
+    StudyStep(type: StudyStepType.measuring, duration: 30, signalFrame: true, measuringTimes: [2,3]),
+    StudyStep(type: StudyStepType.instruction, heading: "Räuspern", description: "Räuspere dich in regelmäßigen Abständen. Bei jedem Räuspern soll der Action Button betätigt werden"),
+    StudyStep(type: StudyStepType.measuring, duration: 30, actionButton: true),
+
   ];
 }
 
 class Dataset2Protocol extends StudyProtocol {
   @override
   List<StudyStep> getSteps() => [
-    StudyStep(type: StudyStepType.instruction, description: "Erste Task Set 2"),
-    StudyStep(type: StudyStepType.measuring, duration: 30),
+    StudyStep(type: StudyStepType.instruction, heading: "Individuell", description: "Betätige während der Reaktion den Action Button. Stelle sicher, dass in der Measurement ID das gemessesne Sympton hinterlegt ist"),
+    StudyStep(type: StudyStepType.measuring, duration: 60, actionButton: true),
   ];
 }
