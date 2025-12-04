0039947a37a4df4623b03886a9d1ee8cbe9e6de8
uqrjd <uqrjd@student.kit.edu>
2025-12-04 10:06:37 +0100

    adjustes studyprotocolls with new pumping text

--- open_wearable/lib/apps/allergy_symptom_tracker/model/study_protocol.dart
+++ open_wearable/lib/apps/allergy_symptom_tracker/model/study_protocol.dart
@@ -271,7 +271,7 @@ class Dataset1Protocol extends StudyProtocol {
             "keep performing the movement",
           ],
           repetitions: 3,
-          practiceText: "you can now practice the pumping movement",
+          practiceText: "you can now practice the saliva movement",
         ),
         StudyStep(
           type: StudyStepType.instruction,
@@ -592,7 +592,7 @@ class Dataset2Protocol extends StudyProtocol {
           type: StudyStepType.instruction,
           heading: "Pumping movement",
           description: "Use your tongue to move saliva over your palate. "
-              "The signal frame around the display indicates when you should perform the pumping movement. "
+              "The signal frame around the display indicates when you should perform the saliva movement. "
               "Perform the action throughout the entire green phase. "
               "Behave normally during the red phases. \n"
               "Recording Type: Frame indication",
@@ -604,11 +604,11 @@ class Dataset2Protocol extends StudyProtocol {
           duration: 62,
           measuringTimes: [5, 5, 4, 5, 3, 5, 4, 5, 2, 5, 4, 5, 3, 5, 4],
           measuringInstructions: [
-            "get ready to for saliva pumping",
-            "keep saliva pumping",
+            "get ready to for saliva movement",
+            "keep performing the movement",
           ],
           repetitions: 3,
-          practiceText: "you can now practice the pumping movement",
+          practiceText: "you can now practice the saliva movement",
         ),
         StudyStep(
           type: StudyStepType.instruction,
